20839368c3e76d0ba9dc0093a48661c8df405d52
oNionUxx <omerlubko@gmail.com>
2020-11-01 18:47:33 +0200

    Adding 'chat' html file

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,86 @@
-io();
+// Global Variables
+const socket = io();
+
+// DOM Elements
+const $messages = document.querySelector('#messages');
+const $form = document.querySelector('#form');
+const $messageForm = $form.querySelector('#message');
+const $formButton = $form.querySelector('button');
+const $geoLocationButton = document.querySelector('#geo-location');
+const geoLocationLink = document.querySelector('#geo-location-link');
+
+// Templates link-template
+const messagesTemplate = document.querySelector('#message-template').innerHTML;
+const linksTemplate = document.querySelector('#location-message-template').innerHTML;
+
+// Event: Initializing main script
+document.addEventListener('DOMContentLoaded', () => {
+  socket.on('message', (message) => {
+    console.log(message);
+
+    const html = Mustache.render(messagesTemplate, {
+      message: message.text,
+      createdAt: moment(message.createdAt).format('hh:mm a'),
+    });
+
+    $messages.insertAdjacentHTML('beforeend', html);
+  });
+});
+
+socket.on('locationMessage', (message) => {
+  console.log(message.url);
+
+  const html = Mustache.render(linksTemplate, {
+    url: message.url,
+    createdAt: moment(message.createdAt).format('hh:mm a'),
+  });
+
+  $messages.insertAdjacentHTML('beforeend', html);
+});
+
+$form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  $formButton.setAttribute('disabled', 'disabled');
+
+  // disable
+  const message = e.target.elements.message.value;
+
+  socket.emit('sendMessage', message, (error) => {
+    // enable
+    $formButton.removeAttribute('disabled');
+
+    $messageForm.value = '';
+    $messageForm.focus();
+
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log('Message delivered!');
+  });
+});
+
+$geoLocationButton.addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser');
+  }
+
+  // disable
+  $geoLocationButton.setAttribute('disabled', 'disabled');
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      'sendLocation',
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        // enable
+        $geoLocationButton.removeAttribute('disabled');
+        console.log('Location shared');
+      }
+    );
+  });
+});
--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ const path = require('path');
 // NPM NODE MODULES
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 const chalk = require('chalk');
+
+// Imports
+const { generateMessage, generateLocationMessage } = require('./utils/messages.js');
 
 const app = express();
 const server = http.createServer(app);
@@ -19,8 +23,34 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 //  Set static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
   console.log(chalk.yellow('New WebSocket connection!'));
+
+  socket.emit('message', generateMessage('Welcome!'));
+  socket.broadcast.emit('message', generateMessage('A new user has joined'));
+
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed!');
+    }
+
+    io.emit('message', generateMessage(message));
+    callback('Message delivered!');
+  });
+
+  // Event: display user location url
+  socket.on('sendLocation', (coords, callback) => {
+    const url = `https://google.com/maps?q=${coords.latitude},${coords.longitude}`;
+    io.emit('locationMessage', generateLocationMessage(url));
+    callback();
+  });
+
+  // Event: 'disconnect'
+  socket.on('disconnect', () => {
+    io.emit('message', generateMessage('A user has left'));
+  });
 });
 
 server.listen(PORT, () => {
