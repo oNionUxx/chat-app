0f9f257fc89747c4d22cf1ad6211ff4eccc78ffc
oNionUxx <omerlubko@gmail.com>
2020-11-03 19:42:25 +0200

    rendering users list

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,12 +1,14 @@
-const generateMessage = (text) => {
+const generateMessage = (user, text) => {
   return {
+    user,
     text,
     createdAt: new Date().getTime(),
   };
 };
 
-const generateLocationMessage = (url) => {
+const generateLocationMessage = (user, url) => {
   return {
+    user,
     url,
     createdAt: new Date().getTime(),
   };
